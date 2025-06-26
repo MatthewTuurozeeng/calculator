8db7e7ccf2b2a78b76d32745749bf45d6534084c
Matthew Tuurozeeng <198502298+MatthewTuurozeeng@users.noreply.github.com>
2025-06-26 15:52:37 +0000

    Completed implementation of the calculator functionality and added keyboard input functionality

--- script.js
+++ script.js
@@ -32,16 +32,11 @@ function divide(x,y){
 
 function operate(operator, x, y) {
   switch (operator) {
-    case '+': 
-        return add(x, y);
-    case '-': 
-        return subtract(x, y);
-    case '*': 
-        return multiply(x, y);
-    case '/': 
-        return divide(x, y);
-    default:
-             return null;
+    case '+': return add(x, y);
+    case '-': return subtract(x, y);
+    case '*': return multiply(x, y);
+    case '/': return divide(x, y);
+    default: return null;
   }
 }
 
@@ -53,38 +48,20 @@ let reset_display = false; // a flag to tel the calculator to clear th display s
 
 
 const display  = document.getElementById('display');
-const number_buttons = document.querySelectorAll(['data-number']);
-const operator_buttons = document.querySelectorAll(['data-operator']);
+const number_buttons = document.querySelectorAll('[data-number]');
+const operator_buttons = document.querySelectorAll('[data-operator]');
 const clear_button = document.getElementById('clear');
 const equals_button = document.getElementById('equals');
-const decimal_button = document.querySelector(['data-decimal']);
+const decimal_button = document.querySelector('[data-decimal]');
 const backspace_button = document.getElementById('backspace');
 
-// append digit to display
-number_buttons.forEach(button =>
-    button.addEventListener('click', () => append_number(button.textContent))
-);
-// operator
-operator_buttons.forEach(button => 
-    button.addEventListener('click', () => set_operation(button.textContent))
-);
-
-function clear() {
-  display.textContent = '0';
-  first_number = '';
-  second_number = '';
-  current_operator = null;
-}
-
-equals_button.addEventListener('click', evalaute);
-clear_button.addEventListener('click', clear);
-decimal_button.addEventListener('click', append_decimal);
-backspace_button.addEventListener('click', backspace);
 
 function resetDisplay() {
   display.textContent = '';
   reset_display = false;
 }
+
+
 
 function appendNumber(number) {
   if (display.textContent === '0' || reset_display) {
@@ -94,4 +71,72 @@ function appendNumber(number) {
 }
 
 
+function evaluate() {
+  if (current_operator === null || reset_display) {
+    return;
+  }
+  if (current_operator === '/' && display.textContent === '0') {
+    display.textContent = "Error! Division by 0";
+    return;
+  }
+  second_number = display.textContent;
+  const result = operate(current_operator, parseFloat(first_number),parseFloat(second_number));
+  display.textContent = Math.round(result * 1000) / 1000;
+  current_operator = null;
+}
+
+function setOperation(operator){
+    if(current_operator !== null){
+        evaluate();
+    }
+    first_number  = display.textContent;
+    current_operator = operator;
+    reset_display = true;
+}
+
+
+function appendDecimal(){
+    if(!display.textContent.includes('.')){
+        display.textContent += '.'
+    }  
+}
+
+// append digit to display
+number_buttons.forEach(button =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+// operator
+operator_buttons.forEach(button => 
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+function clear() {
+  display.textContent = '0';
+  first_number = '';
+  second_number = '';
+  current_operator = null;
+}
+
+function backspace(){
+    display.textContent = display.textContent.slice(0,-1) || '0';
+}
+
+equals_button.addEventListener('click', evaluate);
+clear_button.addEventListener('click', clear);
+decimal_button.addEventListener('click', appendDecimal);
+backspace_button.addEventListener('click', backspace);
+
+
+// keyboard support
+
+function handleKeyboardInput(input) {
+  if (input.key >= 0 && input.key <= 9) appendNumber(input.key);
+  if (input.key === '.') appendDecimal();
+  if (input.key === '=' ||input.key === 'Enter') evaluate();
+  if (input.key === 'Backspace') backspace();
+  if (input.key === 'Escape') clear();
+  if (['+', '-', '*', '/'].includes(input.key)) setOperation(input.key);
+}
+
+window.addEventListener('keydown', handleKeyboardInput); 
 
